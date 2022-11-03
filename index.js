1d450e0f5aa1e3d6adeaa03c5eda3647d3b06c89
Raenique27 <raeniquewalters@gmail.com>
2022-11-03 10:11:18 -0400

    required constructors in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+
+
 
